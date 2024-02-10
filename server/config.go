07db8edb9fee3de8cae79239858f3b9ae409717b
SeraphJACK <seraphjack@outlook.com>
2024-02-10 17:24:33 +0800

    RedPacket endpoint

---
+++ server/config.go
@@ -0,0 +1,6 @@
+package server
+
+import "os"
+
+var RedPacketCode = os.Getenv("RED_PACKET_PASSWORD")
+var RedPacketResult = os.Getenv("RED_PACKET_RESULT")
--- server/server.go
+++ server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"encoding/json"
 	"flag"
 	"log"
 	"net/http"
@@ -11,6 +12,24 @@ var listen = flag.String("listen", "0.0.0.0:8081", "HTTP Server listen address")
 func Start() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/api/completion", handleCompletion)
+	mux.HandleFunc("/api/packet", handleGetRedPacket)
 
 	log.Fatal(http.ListenAndServe(*listen, mux))
+}
+
+func handleGetRedPacket(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	code := r.URL.Query().Get("code")
+	if code != RedPacketCode {
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	_ = json.NewEncoder(w).Encode(RedPacketResult)
 }
